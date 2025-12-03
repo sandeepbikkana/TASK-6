044a3fbe38bb282ce3d93761153fc04b59657479
Lovepreet Singh <95311620+lovepreet976@users.noreply.github.com>
2025-12-03 16:08:10 +0530

    Task 1- strapi application local deployment setup

---
+++ src/api/new-collection/controllers/new-collection.ts
@@ -0,0 +1,7 @@
+/**
+ * new-collection controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::new-collection.new-collection');
---
+++ src/api/new-collection/services/new-collection.ts
@@ -0,0 +1,7 @@
+/**
+ * new-collection service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::new-collection.new-collection');
