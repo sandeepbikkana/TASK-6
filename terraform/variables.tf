61a5afaa124f3ecc589fa58e730e5903f635bc46
sandeepbikkana <sandeepbikkana@gmail.com>
2025-12-11 16:25:23 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,8 +8,9 @@ variable "image_tag" { type = string }
 
 variable "db_name" { default = "strapi" }
 variable "db_username" { default = "strapi" }
-variable "db_password" { default = "Strapi@1234" }
+variable "db_password" { default = "StrapiDB123!" }
 
 variable "key_name" { default = "" }
 variable "project" {default = "sandeep"}
 variable "ACCOUNT_ID" { default = "301782007642" }
+
